378f0e8c4a4ea418f92e48ac9bb494b0af1f74da
toby petty <tobypetty@hotmail.com>
2019-07-21 15:27:41 -0400

    add_symbol method

--- __init__.py
+++ __init__.py
@@ -1,8 +1,23 @@
-from .scrapers import Get
+import yaml
+
+from .nav import SYMBOL_PATH
 from .query import Query
+from .scrape import Get
 
 
 class Tradez:
 
     Get = Get
     Query = Query
+
+    def add_symbol(*sym):
+        """Add stock symbols to the saved list. Doesn't validate to check if 
+        symbols are valid stocks.
+        """
+        with open(SYMBOL_PATH) as stream:
+            symbols = yaml.safe_load(stream)["symbols"]
+        new = sorted(set(sym) - set(symbols))
+        symbols = sorted(set(symbols) | set([str.upper(s) for s in sym]))
+        with open(SYMBOL_PATH, "w") as stream:
+            yaml.safe_dump({"symbols": symbols}, stream)
+        print(f"Added symbols to target list: {', '.join(new)}")
--- nav.py
+++ nav.py
@@ -7,6 +7,6 @@ DIR_DATA = os.path.join(DIR, "datastore")
 DIR_META = os.path.join(DIR, "metadata")
 DIR_SCRAPERS = os.path.join(DIR, "scrapers")
 
-_symbols = os.path.join(DIR_META, "symbols.yaml")
-with open(_symbols, "r") as stream:
+SYMBOL_PATH = os.path.join(DIR_META, "symbols.yaml")
+with open(SYMBOL_PATH, "r") as stream:
     SYMBOLS = yaml.safe_load(stream)["symbols"]
