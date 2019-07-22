a7d001f24a4c68a0d84e51fa03ce2198192da6e2
toby petty <tobypetty@hotmail.com>
2019-07-21 20:45:42 -0400

    remove_symbols method

--- __init__.py
+++ __init__.py
@@ -10,8 +10,12 @@ class Tradez:
     Get = Get
     Query = Query
 
-    def add_symbol(*sym):
-        """Add stock symbols to the saved list. Doesn't validate to check if 
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_symbol(*sym: str):
+        """Add stock symbols to the saved list. Doesn't validate to check if
         symbols are valid stocks.
         """
         with open(SYMBOL_PATH) as stream:
@@ -21,3 +25,22 @@ class Tradez:
         with open(SYMBOL_PATH, "w") as stream:
             yaml.safe_dump({"symbols": symbols}, stream)
         print(f"Added symbols to target list: {', '.join(new)}")
+
+    @staticmethod
+    def remove_symbol(*sym: str):
+        """Remove stock symbols from the saved list.
+        """
+        sym = [s.upper() for s in sym]
+        with open(SYMBOL_PATH) as stream:
+            symbols = yaml.safe_load(stream)["symbols"]
+        remove = sorted(set(sym) & set(symbols))
+        symbols = sorted(set(symbols) - set(sym))
+        with open(SYMBOL_PATH, "w") as stream:
+            yaml.safe_dump({"symbols": symbols}, stream)
+        print(f"Removed symbols from target list: {', '.join(remove)}")
+
+    @property
+    def symbols(self):
+        with open(SYMBOL_PATH, "r") as stream:
+            return yaml.safe_load(stream)["symbols"]
+
