36c731db56b6db8afee6e1a3874d0d160c6b45aa
toby petty <tobypetty@hotmail.com>
2019-07-21 20:48:32 -0400

    syntax consistency

--- __init__.py
+++ __init__.py
@@ -18,13 +18,14 @@ class Tradez:
         """Add stock symbols to the saved list. Doesn't validate to check if
         symbols are valid stocks.
         """
+        sym = [str.upper(s) for s in sym]
         with open(SYMBOL_PATH) as stream:
             symbols = yaml.safe_load(stream)["symbols"]
-        new = sorted(set(sym) - set(symbols))
-        symbols = sorted(set(symbols) | set([str.upper(s) for s in sym]))
+        add = sorted(set(sym) - set(symbols))
+        symbols = sorted(set(symbols) | set(sym))
         with open(SYMBOL_PATH, "w") as stream:
             yaml.safe_dump({"symbols": symbols}, stream)
-        print(f"Added symbols to target list: {', '.join(new)}")
+        print(f"Added symbols to target list: {', '.join(add)}")
 
     @staticmethod
     def remove_symbol(*sym: str):
